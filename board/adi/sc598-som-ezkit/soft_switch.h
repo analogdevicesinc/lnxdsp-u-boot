f6d8f8160a6e8a353acfc88343a338e61f21fc9b
Nathan Barrett-Morrison <nathan.morrison@timesys.com>
2021-10-19 13:02:55 -0400

    SC598: Clean up soft switch configurations and make them more human readable

--- board/adi/sc598-som-ezkit/soft_switch.h
+++ board/adi/sc598-som-ezkit/soft_switch.h
@@ -7,121 +7,97 @@
  */
 
 #include <asm/arch/soft_switch.h>
-
+#include "soft_switch_bits.h"
 #define NUM_SWITCH      2
 
-#undef MX66LM1G45 /* use IS25LP512 */
+#define ADI_USE_MACRONIX_OSPI 0
+
+#define PortA_Address22                                                       \
+		DISABLE_ADAU1979                                              | \
+		DISABLE_ADAU1962                                              | \
+		ENABLE_ADAU_RESET                                             | \
+		DISABLE_CAN                                                   | \
+		(ADI_USE_MACRONIX_OSPI ? DISABLE_FTDI_USB : ENABLE_FTDI_USB)  | \
+		DISABLE_MICROSD_SPI                                           | \
+		DISABLE_PB                                                    | \
+		DISABLE_EEPROM
+
+#define PortB_Address22_EthEnable         \
+		DISABLE_GIGE_RESET      | \
+		DISABLE_ETH1_RESET      | \
+		ENABLE_ETH1_EN
+
+#define PortB_Address22_EthDisable         \
+		ENABLE_GIGE_RESET        | \
+		ENABLE_ETH1_RESET        | \
+		DISABLE_ETH1_EN
+
+#define PortB_Address22                  \
+		DISABLE_MLB            | \
+		DISABLE_AUDIO_JACK_SEL | \
+		DISABLE_SPDIF_OPTICAL  | \
+		DISABLE_SPDIF_DIGITAL  | \
+		(ADI_USE_MACRONIX_OSPI ? ENABLE_OCTAL_SPI_CS : DISABLE_OCTAL_SPI_CS)
+
+#define PortA_Address20                                                               \
+		DISABLE_UART0_FLOW                                                   | \
+		(ADI_USE_MACRONIX_OSPI ? DISABLE_UART0 : ENABLE_UART0)               | \
+		(ADI_USE_MACRONIX_OSPI ? DISABLE_SPI2D2_D3 : ENABLE_SPI2D2_D3)       | \
+		(ADI_USE_MACRONIX_OSPI ? DISABLE_SPI2FLASH_CS : ENABLE_SPI2FLASH_CS) | \
+		ENABLE_LED3                                                          | \
+		ENABLE_LED2                                                          | \
+		ENABLE_LED1
+
+#define PortB_Address20             \
+		ENABLE_EMMC_SOM   | \
+		DISABLE_EMMC
 
 static struct switch_config switch_config_array_ethernet_enabled[NUM_SWITCH] = {
 	{
-/*
-	U6 Port A                      U6 Port B
-
-	7--------------- ~ADAU1979_EN   |  7--------------- ~GIGE_RESET
-	| 6------------- ~ADAU1962_EN   |  | 6------------- ~ETH1_RESET
-	| | 5----------- ~ADAU_RESET    |  | | 5----------- ~ETH1_EN
-	| | | 4--------- ~CAN_EN        |  | | | 4--------- ~MLB_EN
-	| | | | 3------- ~FTDI_USB_EN   |  | | | | 3------- AUDIO_JACK_SEL
-	| | | | | 2----- ~MICROSD_SPI   |  | | | | | 2----- ~SPDIF_OPTICAL_EN
-	| | | | | | 1--- PB_EN          |  | | | | | | 1--- ~SPDIF_DIGITAL_EN
-	| | | | | | | 0- EEPROM_EN      |  | | | | | | | 0- OCTAL_SPI_CS_EN
-	| | | | | | | |                 |  | | | | | | | |
-	O O O O O O O O                 |  O O O O O O O O   (I/O direction)
-	1 1 1 1 0 1 0 0                 |  1 0 1 1 0 1 1 0   (value being set)
-*/
 		.i2c_bus = 2,
 		.i2c_addr = 0x22,
 		.dir0 = 0x0, /* all output */
 		.dir1 = 0x0, /* all output */
-		.value0 = 0xF4,
-		.value1 = 0xB6,
+		.value0 = PortA_Address22,
+		.value1 = PortB_Address22 | PortB_Address22_EthEnable,
 		.is23018 = 0x0,
 		.pullup0 = 0x0,
 		.pullup1 = 0x0,
 	},
-
 	{
-/*
-	U16 Port A                      U16 Port B
-
-	7--------------- Unused         |  7--------------- Unused
-	| 6------------- ~UART0_FLOW_EN |  | 6------------- Unused
-	| | 5----------- ~UART0_EN      |  | | 5----------- Unused
-	| | | 4--------- ~SPI2D2_D3_EN  |  | | | 4--------- Unused
-	| | | | 3------- ~SPI2FLASH_CS  |  | | | | 3------- Unused
-	| | | | | 2----- LED            |  | | | | | 2----- Unused
-	| | | | | | 1--- LED            |  | | | | | | 1--- ~EMMC_SOM_EN
-	| | | | | | | 0- LED            |  | | | | | | | 0- ~EMMC_EN
-	| | | | | | | |                 |  | | | | | | | |
-	O O O O O O O O                 |  O O O O O O O O   (I/O direction)
-	0 0 0 0 0 1 1 1                 |  0 0 0 0 0 0 1 0   (value being set)
-*/
 		.i2c_bus = 2,
 		.i2c_addr = 0x20,
 		.dir0 = 0x0, /* all output */
 		.dir1 = 0x0, /* all output */
-		.value0 = 0x07,
-		.value1 = 0x2,
+		.value0 = PortA_Address20,
+		.value1 = PortB_Address20,
 		.is23018 = 0x1,
-		.pullup0 = 0x07,
-		.pullup1 = 0x02,
+		.pullup0 = PortA_Address20,
+		.pullup1 = PortB_Address20,
 	},
-
 };
 
 static struct switch_config switch_config_array_ethernet_disabled[NUM_SWITCH] = {
 	{
-/*
-	U6 Port A                      U6 Port B
-
-	7--------------- ~ADAU1979_EN   |  7--------------- ~GIGE_RESET
-	| 6------------- ~ADAU1962_EN   |  | 6------------- ~ETH1_RESET
-	| | 5----------- ~ADAU_RESET    |  | | 5----------- ~ETH1_EN
-	| | | 4--------- ~CAN_EN        |  | | | 4--------- ~MLB_EN
-	| | | | 3------- ~FTDI_USB_EN   |  | | | | 3------- AUDIO_JACK_SEL
-	| | | | | 2----- ~MICROSD_SPI   |  | | | | | 2----- ~SPDIF_OPTICAL_EN
-	| | | | | | 1--- PB_EN          |  | | | | | | 1--- ~SPDIF_DIGITAL_EN
-	| | | | | | | 0- EEPROM_EN      |  | | | | | | | 0- OCTAL_SPI_CS_EN
-	| | | | | | | |                 |  | | | | | | | |
-	O O O O O O O O                 |  O O O O O O O O   (I/O direction)
-	1 1 1 1 0 1 0 0                 |  0 0 1 1 0 1 1 0   (value being set)
-*/
 		.i2c_bus = 2,
 		.i2c_addr = 0x22,
 		.dir0 = 0x0, /* all output */
 		.dir1 = 0x0, /* all output */
-		.value0 = 0xF4,
-		.value1 = 0x36,
+		.value0 = PortA_Address22,
+		.value1 = PortB_Address22 | PortB_Address22_EthDisable,
 		.is23018 = 0x0,
 		.pullup0 = 0x0,
 		.pullup1 = 0x0,
 	},
-
 	{
-/*
-	U16 Port A                      U16 Port B
-
-	7--------------- Unused         |  7--------------- Unused
-	| 6------------- ~UART0_FLOW_EN |  | 6------------- Unused
-	| | 5----------- ~UART0_EN      |  | | 5----------- Unused
-	| | | 4--------- ~SPI2D2_D3_EN  |  | | | 4--------- Unused
-	| | | | 3------- ~SPI2FLASH_CS  |  | | | | 3------- Unused
-	| | | | | 2----- LED            |  | | | | | 2----- Unused
-	| | | | | | 1--- LED            |  | | | | | | 1--- ~EMMC_SOM_EN
-	| | | | | | | 0- LED            |  | | | | | | | 0- ~EMMC_EN
-	| | | | | | | |                 |  | | | | | | | |
-	O O O O O O O O                 |  O O O O O O O O   (I/O direction)
-	0 0 0 0 0 0 0 0                 |  0 0 0 0 0 0 1 0   (value being set)
-*/
 		.i2c_bus = 2,
 		.i2c_addr = 0x20,
 		.dir0 = 0x0, /* all output */
 		.dir1 = 0x0, /* all output */
-		.value0 = 0x0,
-		.value1 = 0x2,
+		.value0 = PortA_Address20,
+		.value1 = PortB_Address20,
 		.is23018 = 0x01,
-		.pullup0 = 0x07,
-		.pullup1 = 0x02,
+		.pullup0 = PortA_Address20,
+		.pullup1 = PortB_Address20,
 	},
-
 };
