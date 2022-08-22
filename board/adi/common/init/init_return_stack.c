db55e95722e71cab95a4517a03cc54e7b496224a
Nathan Barrett-Morrison <nathan.morrison@timesys.com>
2022-08-22 11:11:16 -0400

    All boards: Include proper header in init_return_stack.c

--- board/adi/common/init/init_return_stack.c
+++ board/adi/common/init/init_return_stack.c
@@ -1,5 +1,13 @@
 #include <asm/arch/portmux.h>
+#if defined(CONFIG_SC59X_64)
+#include <asm/arch/sc59x_64.h>
+#elif defined(CONFIG_SC59X)
 #include <asm/arch/sc59x.h>
+#elif defined(CONFIG_SC58X)
+#include <asm/arch/sc58x.h>
+#elif defined(CONFIG_SC57X)
+#include <asm/arch/sc57x.h>
+#endif
 #include <asm/io.h>
 #include <config.h>
 #include "init.h"
