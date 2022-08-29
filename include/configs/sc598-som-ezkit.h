5048dc3313e52528b331dc198cd88eaca2f7febe
Greg Malysa <gmalysa@alumni.stanford.edu>
2022-08-29 18:14:50 -0400

    sc598: remove net configs that don't apply to dwmac4

--- include/configs/sc598-som-ezkit.h
+++ include/configs/sc598-som-ezkit.h
@@ -95,10 +95,6 @@
 #define CONFIG_DTBNAME		"sc598-som-ezkit.dtb"
 #define CONFIG_HOSTNAME		"sc59x"
 #define CONFIG_DW_PORTS		1
-#define CONFIG_DW_ALTDESCRIPTOR
-#define CONFIG_DW_AXI_BURST_LEN 16
-#define CONFIG_MII
-//#define CONFIG_PHYLIB
 #define CONFIG_PHY_TI
 #define CONFIG_ETHADDR	02:80:ad:20:31:e8
 #define CONFIG_ETH1ADDR	02:80:ad:20:31:e9
