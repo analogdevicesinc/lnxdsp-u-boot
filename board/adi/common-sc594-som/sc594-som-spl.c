aeb615525f68e0015364338f233043e2377e7e47
Nathan Barrett-Morrison <nathan.morrison@timesys.com>
2023-04-17 15:42:59 -0400

    SC594-EZKit: Bring back Falcon mode support (for SPI/OSPI)

--- board/adi/common-sc594-som/sc594-som-spl.c
+++ board/adi/common-sc594-som/sc594-som-spl.c
@@ -15,7 +15,7 @@ const struct adi_boot_args adi_rom_boot_args[] = {
 	// JTAG/no boot
 	[0] = {0, 0, 0},
 	// SPI master, used for qspi as well
-	[1] = {0x60020000, 0x00040000, 0x00000207},
+	[1] = {0x60040000, 0x00040000, 0x00000207},
 	// SPI slave
 	[2] = {0, 0, 0x00000212},
 	// UART slave
@@ -23,7 +23,7 @@ const struct adi_boot_args adi_rom_boot_args[] = {
 	// Linkport slave
 	[4] = {0, 0, 0x00000014},
 	// OSPI master
-	[5] = {0x60020000, 0, 0x00000008},
+	[5] = {0x60040000, 0, 0x00000008},
 	// reserved, no boot
 	[6] = {0, 0, 0},
 	// reserved, also no boot
