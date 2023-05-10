668ccaf6f07186eb4c1b08845a13cb2ec34104d1
Nathan Barrett-Morrison <nathan.morrison@timesys.com>
2023-05-10 12:40:45 -0400

    arch: arm: adi: Add in Analog Devices SC598 processor and board support files
    Co-developed-by: Greg Malysa <greg.malysa@timesys.com>
    Signed-off-by: Greg Malysa <greg.malysa@timesys.com>
    Signed-off-by: Trevor Woerner <twoerner@gmail.com>
    Signed-off-by: Nathan Barrett-Morrison <nathan.morrison@timesys.com>

---
+++ board/adi/common-sc598-som/sc598-som-spl.c
@@ -0,0 +1,72 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * (C) Copyright 2022 - Analog Devices, Inc.
+ *
+ * Written and/or maintained by Timesys Corporation
+ *
+ * Contact: Nathan Barrett-Morrison <nathan.morrison@timesys.com>
+ * Contact: Greg Malysa <greg.malysa@timesys.com>
+ *
+ * U-boot - SPL management
+ *
+ */
+
+#include <asm/arch-adi/sc5xx/spl.h>
+
+// Table 43-14 in SC598 hardware reference manual
+const struct adi_boot_args adi_rom_boot_args[] = {
+	// JTAG/no boot
+	[0] = {0, 0, 0},
+	// SPI master, used for qspi as well
+	[1] = {0x60040000, 0x00040000, 0x00000207},
+	// SPI slave
+	[2] = {0, 0, 0x00000212},
+	// UART slave
+	[3] = {0, 0, 0x00000013},
+	// Linkport slave
+	[4] = {0, 0, 0x00000014},
+	// OSPI master
+	[5] = {0x60040000, 0, 0x00000008},
+	// eMMC
+	[6] = {0x201, 0, 0x86009},
+	// reserved, also no boot
+	[7] = {0, 0, 0}
+};
+
+unsigned long spl_mmc_get_uboot_raw_sector(struct mmc *mmc,
+					   unsigned long raw_sect)
+{
+	unsigned int mmc_sector_offs = 0;
+
+#if CONFIG_IS_ENABLED(OF_CONTROL) && !CONFIG_IS_ENABLED(OF_PLATDATA)
+    mmc_sector_offs = fdtdec_get_config_int(gd->fdt_blob,
+					    "u-boot,spl-mmc-sector-offset",
+					 mmc_sector_offs);
+#endif
+
+	return mmc_sector_offs;
+}
+
+#ifdef CONFIG_ADI_FALCON
+void spl_board_prepare_ethernet(void)
+{
+	// select RGMII, little endian for both ports
+	writel((readl(REG_PADS0_PCFG0) | 0xc), REG_PADS0_PCFG0);
+	writel(readl(REG_PADS0_PCFG0) & ~(1 << 19), REG_PADS0_PCFG0);
+	writel(readl(REG_PADS0_PCFG0) & ~(1 << 20), REG_PADS0_PCFG0);
+}
+
+void spl_board_prepare_for_linux(void)
+{
+	spl_board_prepare_ethernet();
+	adi_disable_ospi(1);
+}
+
+#if defined(CONFIG_SPL_LOAD_FIT)
+int board_fit_config_name_match(const char *name)
+{
+	return 0;
+}
+#endif
+
+#endif
