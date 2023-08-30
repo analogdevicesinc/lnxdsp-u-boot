f5792f04c2e24cb8c3a20daf42ed6dffa98e83fe
Greg Malysa <gmalysa@alumni.stanford.edu>
2023-08-30 14:33:31 -0400

    adi: migrate spl bootrom hooks to processor-specific spl support with default noop hook

--- arch/arm/include/asm/arch-adi/sc5xx/spl.h
+++ arch/arm/include/asm/arch-adi/sc5xx/spl.h
@@ -30,21 +30,25 @@ struct adi_boot_args {
 
 #if defined(CONFIG_SC59X)
 	#define pRCU_STAT           ((void __iomem *)0x3108C004)
+	#define pRCU_MSG            ((void __iomem *)0x3108C06C)
 	#define pREG_PADS0_PCFG0    ((void __iomem *)0x31004604)
 	#define pREG_PADS0_PCFG1    ((void __iomem *)0x31004608)
 	#define ADI_ROM_ADDRESS		((void (*)(void *, uint32_t, int32_t, void *, uint32_t))0x000000e9)
 #elif defined(CONFIG_SC59X_64)
 	#define pRCU_STAT           ((void __iomem *)0x3108C004)
+	#define pRCU_MSG            ((void __iomem *)0x3108C06C)
 	#define pREG_EMSI0_SWRST    ((void __iomem *)0x310C702F)
 	#define pREG_PADS0_PCFG0    ((void __iomem *)0x31004604)
 	#define pREG_PADS0_PCFG1    ((void __iomem *)0x31004608)
 	#define ADI_ROM_ADDRESS		((void (*)(void *, uint32_t, int32_t, void *, uint32_t))0x000000e4)
 #elif defined(CONFIG_SC57X)
 	#define pRCU_STAT           ((void __iomem *)0x3108C004)
+	#define pRCU_MSG            ((void __iomem *)0x3108C06C)
 	#define pREG_PADS0_PCFG0    ((void __iomem *)0x31004404)
 	#define ADI_ROM_ADDRESS		((void (*)(void *, uint32_t, int32_t, void *, uint32_t))0x000000e1)
 #elif defined(CONFIG_SC58X)
 	#define pRCU_STAT			((void __iomem *)0x3108B004)
+	#define pRCU_MSG            ((void __iomem *)0x3108B06C)
 	#define pREG_PADS0_PCFG0	((void __iomem *)0x31004404)
 	#define ADI_ROM_ADDRESS		((void (*)(void *, uint32_t, int32_t, void *, uint32_t))0x000000e1)
 #endif
@@ -54,5 +58,10 @@ extern u32 bmode;
 void adi_check_pushbuttons(bool active1, bool active2);
 
 extern const struct adi_boot_args adi_rom_boot_args[8];
+
+// Struct layout is processor specific
+struct ADI_ROM_BOOT_CONFIG;
+
+int32_t adi_rom_boot_hook(struct ADI_ROM_BOOT_CONFIG *cfg, int32_t cause);
 
 #endif
--- board/adi/common-sc598-som/sc598-som-spl.c
+++ board/adi/common-sc598-som/sc598-som-spl.c
@@ -47,6 +47,82 @@ unsigned long spl_mmc_get_uboot_raw_sector(struct mmc *mmc,
 	return mmc_sector_offs;
 }
 
+struct ADI_ROM_BOOT_CONFIG {
+	void *src;
+	void *dest;
+	int32_t byte_count;
+	int32_t flags;
+	uint32_t block_count;
+	uint32_t block_current;
+	void *next_dxe;
+	uint32_t byte_address;
+	uint32_t *control_register;
+	int32_t control_value;
+	uint32_t *peripheral_base;
+	uint32_t *aux_control_register;
+	uint32_t *aux_peripheral_base;
+	uint32_t *sec_control_register;
+	void *dma_base_register;
+	int32_t load_type;
+	struct {
+		uint32_t operation;
+		uint32_t id;
+		void *src;
+		void *destination;
+		uint32_t byte_count;
+		int32_t done_detect;
+		uint32_t crc_ctl;
+		uint32_t fill_value;
+		uint32_t crc_poly;
+		uint32_t crc_compare;
+	} mdma_cfg;
+	uint16_t data_width;
+	uint16_t src_modify_mult;
+	uint16_t dst_modify_mult;
+	uint16_t usr_short;
+	int32_t user_long;
+	int32_t reserved0;
+	void *mode_data;
+	int32_t boot_command;
+	void *boot_header;
+	void *temp_buffer;
+	void *reserved1;
+	int32_t temp_byte_count;
+	void *temp_src;
+	int32_t page_byte_count;
+	struct {
+		struct {
+			uint8_t *buffer;
+			uint32_t size;
+			uint32_t page_size;
+		} buffer[2];
+		uint32_t state;
+		void *src;
+		void *dma;
+	} boot_buffers;
+	struct {
+		void *init;
+		void *config;
+		void *load;
+		void *cleanup;
+		void *reserved0;
+		int32_t reserved1;
+	} registry;
+	void (*error)(struct ADI_ROM_BOOT_CONFIG *);
+	// @todo there are more fields but this should be enough for what we need to see for now
+};
+
+static void adi_rom_boot_error_handler(struct ADI_ROM_BOOT_CONFIG *config) {
+	printf("bootrom failed, rcu msg = 0x%x\n", readl(pRCU_MSG));
+	while (1)
+		;
+}
+
+int32_t adi_rom_boot_hook(struct ADI_ROM_BOOT_CONFIG *config, int32_t cause) {
+	config->error = &adi_rom_boot_error_handler;
+	return 0;
+}
+
 #ifdef CONFIG_ADI_FALCON
 void spl_board_prepare_ethernet(void)
 {
