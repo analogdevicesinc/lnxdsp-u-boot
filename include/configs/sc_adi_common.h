19b81ea10b32ec365dcc6afa44cc675dd391c6aa
Vasileios Bimpikas <110021710+vasbimpikasadi@users.noreply.github.com>
2023-09-21 14:39:40 +0100

    Restore `tftpserverip`
    Restored `tftpserverip` in `init_ethernet` and will point the documentation to only set `tftpserverip`. `dhcp` will override `serverip` and it's best to not tweak the DHCP functionality, nor point the user to tweak their DHCP server neither.

--- include/configs/sc_adi_common.h
+++ include/configs/sc_adi_common.h
@@ -89,7 +89,7 @@
 #define UBOOT_ENV_FILE "u-boot-" CONFIG_SYS_BOARD ".ldr"
 
 #define ADI_INIT_ETHERNET \
-	"init_ethernet=mii info; dhcp; setenv serverip ${serverip};\0"
+	"init_ethernet=mii info; dhcp; setenv serverip ${tftpserverip};\0"
 
 #define ADI_NFS_BOOT \
 	"nfsargs=setenv bootargs " ADI_BOOTARGS_NFS "\0" \
