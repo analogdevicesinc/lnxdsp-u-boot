e2997f1c408293c12823ffa23eb0a219acbb43de
UtsavAgarwalADI <utsav.agarwal@analog.com>
2024-05-27 12:28:30 +0100

    SC594: Hotfix for SC594 L3 DDR memory overlap
    Resizing accessible memory range for ARM on sc594 such that
    it does not overlap with SHARC allocated address space
    
    Signed-off-by: UtsavAgarwalADI <utsav.agarwal@analog.com>

--- include/configs/sc_adi_common.h
+++ include/configs/sc_adi_common.h
@@ -62,10 +62,18 @@
 
 #define ADI_MMC_BOOTCMD "run mmcargs; " ADI_BOOT ""
 
+//only applicable for SC594
+#if defined CONFIG_SC59X && !CONFIG_SC59X_64
+#define ADI_MEM_SIZE	"mem=512M "
+#else
+#define ADI_MEM_SIZE	""
+#endif
+
 #define ADI_BOOTARGS_CONSOLE \
 		ADI_EARLYPRINTK \
 		"console=ttySC" __stringify(CONFIG_UART_CONSOLE) "," \
-						__stringify(CONFIG_BAUDRATE) " " \
+				__stringify(CONFIG_BAUDRATE) " " \
+				ADI_MEM_SIZE \ 
 		"vmalloc=512M "
 
 #define ADI_BOOTARGS_MMC \
